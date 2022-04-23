9282a27a9d6bcd53ed2a5d244eb5c59ea3e77875
Viollae-Cahaya <95465414+Viollae-Cahaya@users.noreply.github.com>
2022-04-23 13:28:37 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Navbar from "./components/Navbar"
+import Main from "./Main"
+
+ 
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <Navbar />
+                <Main />
+               
+
+            </div>
+        )
+    }
+
+    
+}
+export default App;
